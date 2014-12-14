4c9041cd79b65945a51bd9bbe6033090b3e5b75c
Bharathkumar <bharath2k2in@gmail.com>
2014-12-14 19:08:16 +0530

    Added form tag again to have validation in place

--- web/js/module.js
+++ web/js/module.js
@@ -2,11 +2,16 @@ angular.module('homeExpenseApp', [])
     .controller('loginController', function ($scope, accountService) {
         'use strict';
 
+        function clearInputData() {
+            $scope.username = '';
+            $scope.password = '';
+            $scope.repeatPassword = '';
+        }
+
         $scope.showCreateAccount = false;
         $scope.validationError = false;
-        $scope.username = '';
-        $scope.password = '';
-        $scope.repeatPassword = '';
+
+        clearInputData();
 
         $scope.goToAccountCreationScreen = function () {
             $scope.showCreateAccount = true;
@@ -17,13 +22,15 @@ angular.module('homeExpenseApp', [])
         };
 
         $scope.createAccount = function () {
+
             var requestData = {
                 username: $scope.username,
                 password: $scope.password
             };
-            accountService.createAccount(requestData).then(function(responseData) {
-                if(responseData.response) {
+            accountService.createAccount(requestData).then(function (responseData) {
+                if (responseData.response) {
                     $scope.showCreateAccount = false;
+                    clearInputData();
                 } else {
                     $scope.validationError = true;
                 }
